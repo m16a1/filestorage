3e97a48554eed3e4fd6c6890ceb8c1f6add7462c
m16a1 <a741su@gmail.com>
2013-12-24 20:04:28 +0400

    first commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'rack/test'
+
+require File.expand_path '../../app.rb', __FILE__
+
+module RSpecMixin
+  include Rack::Test::Methods
+  def app
+    described_class
+  end
+end
+
+RSpec.configure { |c| c.include RSpecMixin }

fd7958ee977ad0599e2cfa268855e4d5dfc10d9b
m16a1 <a741su@gmail.com>
2014-01-04 16:16:04 +0400

    new feature: update file

--- controllers/file.rb
+++ controllers/file.rb
@@ -1,18 +1,23 @@
 class FileController < Sinatra::Base
   helpers ApplicationHelper, StatusCodesHelper
-  
+
   get '/*' do
     not_found unless File.exists? abs_path
     File.open(abs_path).read
   end
-  
+
   post '/*' do
     not_allowed if File.exists? abs_path
-    File.open(abs_path, 'w') {|f| f.write(request.body) }
+    File.open(abs_path, 'wb') {|f| f.write(request.body) }
     created
   end
-  
+
   head '/*' do
     not_found unless File.exists? abs_path
+  end
+
+  put '/*' do
+    not_found unless File.exists? abs_path
+    File.open(abs_path, 'wb') {|f| f.write(request.body) }
   end
 end
